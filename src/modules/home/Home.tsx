29a6ddc3d769176ef3e7fa390fda737b4e092674
raggi.izar <raggi.izar@aoatechpartners.com.br>
2023-10-26 14:07:48 -0300

    fix: unuseless div

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -2,15 +2,15 @@ import { Scene } from "./mac/Scene"
 
 export const Home = () => {
   return (
-    <div className="relative flex h-[220vh] w-screen bg-[#1c232e]">
-        <div className="flex flex-col w-screen">
-            <h1 className="font-space font-bold text-[7.5rem] self-center mt-[35vh] text-white">HELLO <span className="text-teal-600">3D</span> WORLD</h1>
-            <div className="absolute h-[130vh] right-0 left-0 m-auto top-[10%] pointer-events-none">
-                <div className="sticky right-0 left-0 m-auto top-[50vh] h-[60vh]">
-                    <Scene />
-                </div>
-            </div>
-        </div>
+    <div className="relative flex flex-col h-[220vh] w-screen bg-[#1c232e]">
+      <h1 className="font-space font-bold text-[7.5rem] self-center mt-[35vh] text-white">
+        HELLO <span className="text-teal-600">3D</span> WORLD
+      </h1>
+      <div className="absolute h-[130vh] right-0 left-0 m-auto top-[10%] pointer-events-none">
+          <div className="sticky right-0 left-0 m-auto top-[50vh] h-[60vh]">
+              <Scene />
+          </div>
+      </div>
     </div>
   )
 }
